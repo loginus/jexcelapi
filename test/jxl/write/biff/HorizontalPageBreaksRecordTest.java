ea8c47c5125dd440b7347eb4f01ee26c0a38afe6
Jan Schlößin <jan.schloessin@incasoft.de>
2015-03-08 20:15:41 +0100

    adds some test for catching the behavoir of page breaks when inserting or removing rows and columns

--- test/jxl/write/biff/HorizontalPageBreaksRecordTest.java
+++ test/jxl/write/biff/HorizontalPageBreaksRecordTest.java
@@ -1,6 +1,11 @@
 package jxl.write.biff;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import jxl.WorkbookSettings;
+import jxl.biff.FormattingRecords;
 import static jxl.biff.Type.HORIZONTALPAGEBREAKS;
+import jxl.write.*;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -16,6 +21,46 @@ public class HorizontalPageBreaksRecordTest {
     HorizontalPageBreaksRecord wpb = new HorizontalPageBreaksRecord(pageBreaks);
     assertArrayEquals(new byte[]{(byte) HORIZONTALPAGEBREAKS.value, 0, 14, 0, 2, 0, 13, 0, 0, 0, (byte) 255, (byte) 255, 18, 0, 0, 0, (byte) 255, (byte) 255},
             wpb.getBytes());
+  }
+  
+  @Test
+  public void testInsertionOfRowBreaks() {
+    WritableSheetImpl w = new WritableSheetImpl("a", null, null, null, null, null);
+    assertArrayEquals(new int[] {}, w.getRowPageBreaks());
+    w.addRowPageBreak(5);
+    assertArrayEquals(new int[] {5}, w.getRowPageBreaks());
+  }
+  
+  @Test
+  public void testDoubleInsertionOfRowBreaks() {
+    WritableSheetImpl w = new WritableSheetImpl("a", null, null, null, null, null);
+    w.addRowPageBreak(5);
+    w.addRowPageBreak(5);
+    assertArrayEquals(new int[] {5}, w.getRowPageBreaks());
+  }
+  
+  @Test
+  public void testInsertionOfRow() throws WriteException, IOException {
+    WritableSheetImpl w = new WritableSheetImpl("[a]", null, new FormattingRecords(null), new SharedStrings(), new WorkbookSettings(), new WritableWorkbookImpl(Files.newOutputStream(Files.createTempFile(null, null)), true, new WorkbookSettings()));
+    w.addCell(new Blank(10, 10));
+    w.addRowPageBreak(5);
+    w.addRowPageBreak(6);
+    w.insertRow(6);
+    assertArrayEquals(new int[] {5,7}, w.getRowPageBreaks());
+    w.insertRow(0);
+    assertArrayEquals(new int[] {6,8}, w.getRowPageBreaks());
+  }
+  
+  @Test
+  public void testRemovalOfRow() throws WriteException, IOException {
+    WritableSheetImpl w = new WritableSheetImpl("[a]", null, new FormattingRecords(null), new SharedStrings(), new WorkbookSettings(), new WritableWorkbookImpl(Files.newOutputStream(Files.createTempFile(null, null)), true, new WorkbookSettings()));
+    w.addCell(new Blank(10, 10));
+    w.addRowPageBreak(6);
+    w.addRowPageBreak(8);
+    w.removeRow(0);
+    assertArrayEquals(new int[] {5,7}, w.getRowPageBreaks());
+    w.removeRow(6);
+    assertArrayEquals(new int[] {5,6}, w.getRowPageBreaks());
   }
   
 }
--- test/jxl/write/biff/VerticalPageBreaksRecordTest.java
+++ test/jxl/write/biff/VerticalPageBreaksRecordTest.java
@@ -1,6 +1,11 @@
 package jxl.write.biff;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import jxl.WorkbookSettings;
+import jxl.biff.FormattingRecords;
 import static jxl.biff.Type.VERTICALPAGEBREAKS;
+import jxl.write.*;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -16,6 +21,46 @@ public class VerticalPageBreaksRecordTest {
     VerticalPageBreaksRecord wpb = new VerticalPageBreaksRecord(pageBreaks);
     assertArrayEquals(new byte[]{(byte) VERTICALPAGEBREAKS.value, 0, 14, 0, 2, 0, 2, 0, 0, 0, (byte) 255, (byte) 255, 8, 0, 0, 0, (byte) 255, (byte) 255},
             wpb.getBytes());
+  }
+  
+  @Test
+  public void testInsertionOfColumnBreaks() {
+    WritableSheetImpl w = new WritableSheetImpl("a", null, null, null, null, null);
+    assertArrayEquals(new int[] {}, w.getColumnPageBreaks());
+    w.addColumnPageBreak(5);
+    assertArrayEquals(new int[] {5}, w.getColumnPageBreaks());
+  }
+  
+  @Test
+  public void testDoubleInsertionOfColumnBreaks() {
+    WritableSheetImpl w = new WritableSheetImpl("a", null, null, null, null, null);
+    w.addColumnPageBreak(5);
+    w.addColumnPageBreak(5);
+    assertArrayEquals(new int[] {5}, w.getColumnPageBreaks());
+  }
+  
+  @Test
+  public void testInsertionOfColumn() throws WriteException, IOException {
+    WritableSheetImpl w = new WritableSheetImpl("[a]", null, new FormattingRecords(null), new SharedStrings(), new WorkbookSettings(), new WritableWorkbookImpl(Files.newOutputStream(Files.createTempFile(null, null)), true, new WorkbookSettings()));
+    w.addCell(new Blank(10, 10));
+    w.addColumnPageBreak(5);
+    w.addColumnPageBreak(6);
+    w.insertColumn(6);
+    assertArrayEquals(new int[] {5,7}, w.getColumnPageBreaks());
+    w.insertColumn(0);
+    assertArrayEquals(new int[] {6,8}, w.getColumnPageBreaks());
+  }
+  
+  @Test
+  public void testRemovalOfColumn() throws WriteException, IOException {
+    WritableSheetImpl w = new WritableSheetImpl("[a]", null, new FormattingRecords(null), new SharedStrings(), new WorkbookSettings(), new WritableWorkbookImpl(Files.newOutputStream(Files.createTempFile(null, null)), true, new WorkbookSettings()));
+    w.addCell(new Blank(10, 10));
+    w.addColumnPageBreak(6);
+    w.addColumnPageBreak(8);
+    w.removeColumn(0);
+    assertArrayEquals(new int[] {5,7}, w.getColumnPageBreaks());
+    w.removeColumn(6);
+    assertArrayEquals(new int[] {5,6}, w.getColumnPageBreaks());
   }
   
 }
